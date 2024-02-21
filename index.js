a8fd03b7d0abcb210f76c6397143f9b1dee8bc6b
Gabrielle <ms.grodarte@gmail.com>
2024-02-21 15:22:12 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+
+function submitData(userName, userEmail){
+    const userData = {
+        name: userName,
+        email: userEmail
+    }
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type":"application/json",
+            "Accept":"application/json",
+        },
+        body: JSON.stringify(userData)
+    })
+    .then(res=>res.json())
+    .then(newUser => {
+        const userID = newUser.id;
+        const element = document.querySelector("body");
+        element.innerHTML = userID
+    })
+    .catch(error => {
+        const message = error.message;
+        const element = document.querySelector("body");
+        element.innerHTML = message
+        }
+    )
+    }
